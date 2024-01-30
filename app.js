75bd2ee7824d80a00c0d98ab21e563a44e7e8758
santi142004 <santiagobarreralopez2004@gmail.com>
2024-01-30 11:30:26 -0500

    back-libreria

---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const cors = require('cors');
+const mysql = require('mysql2');
+
+app.use(express.json());
+app.use(cors());
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'libreria'
+});
+
+
+app.get('/harryBooks', (req, res) => {
+  db.query('SELECT * FROM libro', (err, results) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Error al obtener datos de la base de datos' });
+    } else {
+      res.json(results);
+    }
+  });
+});
+
+app.post('/updatebooks', (req, res) => {
+  const { id_libro, cantidad } = req.body; // Recupera los datos del cuerpo de la solicitud POST
+
+  if (!id_libro || cantidad === undefined) {
+    res.status(400).json({ error: 'Se requiere un id_libro y una cantidad válida.' });
+    return;
+  }
+
+  db.query('UPDATE libro SET cantidad = cantidad + ? WHERE id_libro = ?', [cantidad, id_libro], (err, results) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Error al actualizar la cantidad en la base de datos' });
+    } else {
+      res.json(results);
+    }
+  });
+});
+
+
+
+app.listen(port, () => {
+  console.log(`La API REST está escuchando en el puerto ${port}`);
+});
